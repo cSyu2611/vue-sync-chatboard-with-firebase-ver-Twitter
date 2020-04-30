c0616c86c887db76a22d303b2bb16bdd65febb43
cSyu2611 <littlemoneyseeker@gmail.com>
2020-04-30 09:32:44 +0900

    mod: ログイン時にメールアドレスを表示

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="home">
     ホーム画面
+    <p>ようこそ、{{firebase.auth().currentUser.email}}さん</p>
     <SignOut/>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 import SignOut from '@/components/SignOut'
 export default {
   name: 'Home',
   components: {
     SignOut
+  },
+  data(){
+    return {
+      firebase: firebase
+    }
   },
   methods: {
   }

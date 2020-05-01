fe9a41d0a7d545569c6f6618d9f7b4059f7b5725
cSyu2611 <littlemoneyseeker@gmail.com>
2020-05-01 19:31:33 +0900

    add: 掲示板コンポーネント周りのコメント

--- src/components/ChatBoard.vue
+++ src/components/ChatBoard.vue
@@ -1,22 +1,23 @@
 <template>
+  <!-- 投稿されたコメントのリスト -->
   <v-list three-line>
+    <!-- コメントの数だけtemplateをv-forで回す -->
     <template v-for="(comment, index) in comments">
+      <!-- 本文や日付、ユーザ名のブロック -->
       <v-list-item
           :key="index"
       >
-
         <v-list-item-content>
-          <v-list-item-subtitle v-if="comment.user==firebase.auth().currentUser.email" class="text--primary subheading">あなた：{{comment.content}}</v-list-item-subtitle>
-          <v-list-item-subtitle v-else class="text--primary subheading">{{comment.content}}</v-list-item-subtitle>
+          <!-- コメント本文、投稿者が自分なら「あなた：」から始める -->
+          <v-list-item-subtitle class="text--primary subheading"><div v-if="comment.user==firebase.auth().currentUser.email" class="my_text">あなた</div>：{{comment.content}}</v-list-item-subtitle>
+          <!-- 日付とユーザのメールアドレス -->
           <v-list-item-subtitle>
             {{comment.createdAt.toDate().toLocaleString()}}
             ＜{{comment.user}}＞
           </v-list-item-subtitle>
         </v-list-item-content>
-
-        <v-list-item-action>
-        </v-list-item-action>
       </v-list-item>
+      <!-- 分割するための線 -->
       <v-divider :key="comment.id"></v-divider>
     </template>
   </v-list>
@@ -41,3 +42,9 @@
     },
   }
 </script>
+
+<style>
+.my_text{
+  font-weight: bold;
+}
+</style>
